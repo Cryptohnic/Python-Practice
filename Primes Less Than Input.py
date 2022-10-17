52b69957a1cda4d0295e1675d70d43d78724ef54
Brandon A <brandonwalex@gmail.com>
2022-10-17 15:23:11 -0500

    Update Primes Less Than Input.py

--- Primes Less Than Input.py
+++ Primes Less Than Input.py
@@ -1,10 +1,19 @@
-p=[1,2]
-n=int(input('What is your num?: '))
+from turtle import end_fill
+
+
+plist=[1,2]
+n=int(input('What is your number?: '))
+if n==1:
+    print("There are no prime numbers less than 1")
+    exit()
+if n==2:
+    print('List of prime numbers less than',str(n)+':',plist[0:1])
+    exit()
 for i in range(3,n):
-    f=False
+    prime=True
     for j in range(2,i):
         if i%j==0:
-            f=True
+            prime=False
             break
-    if not f:p.append(i)
-print('List of prime numbers less than',str(n)+':',p)
+    if prime:plist.append(i)
+print('List of prime numbers less than',str(n)+':',plist)
