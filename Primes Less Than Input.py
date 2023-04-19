fa5ea995a19a3d40d221d2c07c6687bf7a118226
Brandon A <brandonwalex@gmail.com>
2023-04-19 08:35:46 -0500

    Update Primes Less Than Input.py

--- Primes Less Than Input.py
+++ Primes Less Than Input.py
@@ -1,10 +1,9 @@
 plist=[]
 n=int(input('What is your number?: '))
 for i in range(2,n):
-    prime=True
     for j in range(2,i):
         if i%j==0:
-            prime=False
             break
-    if prime: plist.append(i)
+    else: 
+        plist.append(i)
 print(f'List of prime numbers less than {n}: {plist}')
